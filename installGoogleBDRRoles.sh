48a7fee67cf31d868d219af663d9221baa415347
Anthony Vandewerdt <34634682+aussiestorblog@users.noreply.github.com>
2022-05-12 15:02:31 +1000

    Update installGoogleBDRRoles.sh

--- installGoogleBDRRoles.sh
+++ installGoogleBDRRoles.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # ONVAULT
-
-YAML_FILE='gbdr-onvault-role-template.yaml'
-YAML_URL=https://raw.githubusercontent.com/Actifio/rolepermissions/main/gbdr-onvault-role-template.yaml
+YAML_FILE='backupdr-onvault-role-template.yaml'
+YAML_URL=https://raw.githubusercontent.com/Actifio/rolepermissions/main/backupdr-onvault-role-template.yaml
 echo "Downloading template for Google Backup and DR IAM role creation"
 curl ${YAML_URL} -o ${YAML_FILE}
 
@@ -18,7 +17,7 @@ gcloud services enable cloudresourcemanager.googleapis.com
 
 # set the variables to be used
 PROJECT_ID=$(gcloud config get-value project)
-ROLE_NAME=GoogleBDROnVault
+ROLE_NAME=BackupDROnVault
 
 echo "Checking if Google Backup and DR IAM role ${ROLE_NAME} already exists in project ${PROJECT_ID}"
 # check if role already exists else create new
@@ -34,10 +33,9 @@ echo
 echo "OnVault Role installed for use with an SA to create OnVault images in Google Cloud Storage"
 echo
 
-# Persistent Disk Snapshots
-
-YAML_FILE='gbdr-gce-instance-backup-role-template.yaml'
-YAML_URL=https://raw.githubusercontent.com/Actifio/rolepermissions/main/gbdr-gce-instance-backup-role-template.yaml
+# GCE Instance Backups
+YAML_FILE='backupdr-gce-instance-backup-role-template.yaml'
+YAML_URL=https://raw.githubusercontent.com/Actifio/rolepermissions/main/backupdr-gce-instance-backup-role-template.yaml
 echo "Downloading template for Google Backup and DR IAM role creation"
 curl ${YAML_URL} -o ${YAML_FILE}
 
@@ -46,7 +44,7 @@ echo "Creating Google Backup and DR GCE Instance backup IAM role, please wait"
 sleep 5
 
 # set the variables to be used
-ROLE_NAME=GoogleBDRGCEInstanceBackup
+ROLE_NAME=BackupDRGCEInstanceBackup
 
 echo "Checking if Google Backup and DR  IAM role ${ROLE_NAME} already exists in project ${PROJECT_ID}"
 # check if role already exists else create new
@@ -60,6 +58,33 @@ else
 fi
 echo
 echo "Persistent Disk Role installed for use with an SA to perform Google Cloud VM Instance Snapshots in Google Cloud"
+echo
+
+# SA API 
+YAML_FILE='backupdr-sa-api-role-template.yaml'
+YAML_URL=https://raw.githubusercontent.com/Actifio/rolepermissions/main/backupdr-sa-api-role-template.yaml
+echo "Downloading template for Google Backup and DR IAM role creation"
+curl ${YAML_URL} -o ${YAML_FILE}
+
+echo
+echo "Creating Google Backup and Service Account API IAM role, please wait"
+sleep 5
+
+# set the variables to be used
+ROLE_NAME=BackupDRServiceAccountRole
+
+echo "Checking if Google Backup and DR  IAM role ${ROLE_NAME} already exists in project ${PROJECT_ID}"
+# check if role already exists else create new
+if gcloud iam roles describe ${ROLE_NAME} --project=${PROJECT_ID} --quiet
+then
+    echo "IAM Role found, running update"
+        gcloud iam roles update ${ROLE_NAME} --project=${PROJECT_ID} --file=${YAML_FILE} --quiet
+else
+    echo "IAM Role not found, creating new"
+        gcloud iam roles create ${ROLE_NAME} --project=${PROJECT_ID} --file=${YAML_FILE} --quiet
+fi
+echo
+echo "Role installed for use with an SA to perform Backup and DR APIs in Google Cloud"
 echo
 
 
